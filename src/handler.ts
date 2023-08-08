1f1a413300f815fcf5cd271ac951ff27988de9f8
qilip <qilip@qilip.io>
2023-08-09 04:26:12 +0900

    🔨 Fix: 오류나면 터져서 DLQ 들어가게 변경. closed EVF-224
    #

--- src/handler.ts
+++ src/handler.ts
@@ -18,20 +18,13 @@ export const handler = async (event) => {
     const documentId = messageBody.documentId;
 
     // DB에서 Docid를 통해 가져오기
-    let doc: any;
-
-    try {
-      doc = await findDoc(documentId);
-      // 중복 처리 방지
-      if (
-        doc?.status !== Status.SCRAPE_PENDING &&
-        doc?.status !== Status.SCRAPE_REJECTED
-      )
-        continue;
-    } catch (e) {
-      console.error(e);
+    const doc = await findDoc(documentId);
+    // 중복 처리 방지
+    if (
+      doc?.status !== Status.SCRAPE_PENDING &&
+      doc?.status !== Status.SCRAPE_REJECTED
+    )
       continue;
-    }
 
     // 해당 Doc의 상태를 처리중으로 변경
     await changeDocStatus(documentId, Status.SCRAPE_PROCESSING);
@@ -53,9 +46,8 @@ export const handler = async (event) => {
       // SQS에 임베딩 요청
       await enqueue(documentId);
     } catch (e) {
-      console.error(e);
       await changeDocStatus(documentId, Status.SCRAPE_REJECTED);
-      continue;
+      throw e;
     }
 
     // DB에 상태 저장
